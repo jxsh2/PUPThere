6ec0b4fa590b0817a01d0f9b85744d4de0ed4174
jxsh2 <92442082+jxsh2@users.noreply.github.com>
2024-07-19 00:04:43 +0800

    big changes

---
+++ Form2.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form2))
+        btnBack = New Button()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnBack
+        ' 
+        btnBack.BackColor = Color.FromArgb(CByte(226), CByte(184), CByte(8))
+        btnBack.BackgroundImageLayout = ImageLayout.None
+        btnBack.FlatAppearance.BorderSize = 0
+        btnBack.FlatStyle = FlatStyle.Flat
+        btnBack.Font = New Font("Corbel", 13F, FontStyle.Regular, GraphicsUnit.Point)
+        btnBack.ForeColor = Color.White
+        btnBack.Location = New Point(1113, 12)
+        btnBack.Name = "btnBack"
+        btnBack.Size = New Size(139, 36)
+        btnBack.TabIndex = 9
+        btnBack.Text = "BACK"
+        btnBack.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImage = My.Resources.Resources.map
+        PictureBox1.Location = New Point(-2, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(1268, 681)
+        PictureBox1.TabIndex = 11
+        PictureBox1.TabStop = False
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(128), CByte(1), CByte(1))
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(1264, 681)
+        Controls.Add(btnBack)
+        Controls.Add(PictureBox1)
+        DoubleBuffered = True
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form2"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "MAP "
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnBack As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
